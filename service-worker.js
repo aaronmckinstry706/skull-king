4b8bd3f8f2972aa567064d01b81a7b7368437fec
Aaron <aaronmckinstry706@gmail.com>
2025-04-13 09:38:32 -0400

    Update service-worker.js
    Fixed paths

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('skull-king-cache').then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/app.js',
-                '/sk_rulebook_thumbsk_rulebook_thumbnail_250x369.png',
+                '/skull-king',
+                '/skull-king/index.html',
+                '/skull-king/style.css',
+                '/skull-king/app.js',
+                '/skull-king/sk_rulebook_thumbsk_rulebook_thumbnail_250x369.png',
                 // Add other assets as needed
             ]);
         })
