e1ac57d8c034af1b5b21a662812ce58ff4b16c75
Aaron <aaronmckinstry706@gmail.com>
2025-04-13 22:07:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -275,12 +275,6 @@ document.addEventListener('DOMContentLoaded', () => {
             this.render();
         },
         bindEvents: function() {
-          document.getElementById("add-round-btn").addEventListener("click", () => {
-            addRound();
-            uiState.expandedRounds.clear();
-            uiState.expandedRounds.add(gameState.rounds.length - 1); // open new one
-            renderAllRounds();
-          });
         },
         render: function() {
             renderPlayerEditor();
